2f285b846fd7ae24ce73c94dd0fc581f4d7b8bcc
Druns <44653657+DrunsR@users.noreply.github.com>
2018-11-05 18:10:39 -0500

    avance

--- Startup.cs
+++ Startup.cs
@@ -56,7 +56,7 @@ namespace Trabajo
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Home}/{action=Nosotros}/{id?}");
             });
         }
     }
