ee8cca1eb6c40a2bfcab56623c668bd913dbc21e
alumno informatica <alumno@ciis.usil.edu.pe>
2018-11-07 16:39:35 -0500

    yo

---
+++ Models/ReadtToEatContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Trabajo.Models
+{
+    public class ReadtToEatContext : DbContext
+    {
+        
+
+        
+        
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -30,6 +30,7 @@ namespace Trabajo
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
+            
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
